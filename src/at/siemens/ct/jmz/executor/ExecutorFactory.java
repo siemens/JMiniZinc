3d7e8ddf5fcd4699bbbf6efcc4fadee7fb9fbf53
Richard Taupe <richard.taupe@siemens.com>
2016-02-16 17:07:49 +0000

    introducing the ExecutorFactory which produces Executors

---
+++ src/at/siemens/ct/jmz/executor/ExecutorFactory.java
@@ -0,0 +1,34 @@
+package at.siemens.ct.jmz.executor;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+import at.siemens.ct.jmz.writer.IModelWriter;
+
+/**
+ * A factory for {@link Executor}s
+ * 
+ * @author z003ft4a (Richard Taupe)
+ *
+ * @param <E>
+ *          the type of Executors produced by this factory
+ */
+public class ExecutorFactory<E extends Executor> {
+
+  private Class<E> executorClass;
+
+  public ExecutorFactory(Class<E> executorClass) {
+    this.executorClass = executorClass;
+  }
+
+  public E createExecutor(IModelWriter modelWriter) {
+    try {
+      Constructor<E> constructor = executorClass.getConstructor(IModelWriter.class);
+      return constructor.newInstance(modelWriter);
+    } catch (NoSuchMethodException | InstantiationException | IllegalAccessException
+        | IllegalArgumentException | InvocationTargetException e) {
+      throw new IllegalStateException(e);
+    }
+  }
+
+}
