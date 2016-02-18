19ca603d2c13060ab3103fa9b441e51a173d0426
Richard Taupe <richard.taupe@siemens.com>
2016-02-18 16:13:14 +0000

    constraints are boolean expressions

--- src/at/siemens/ct/jmz/elements/constraints/Constraint.java
+++ src/at/siemens/ct/jmz/elements/constraints/Constraint.java
@@ -1,8 +1,9 @@
 package at.siemens.ct.jmz.elements.constraints;
 
 import at.siemens.ct.jmz.elements.Element;
+import at.siemens.ct.jmz.expressions.bool.BooleanExpression;
 
-public abstract class Constraint implements Element {
+public abstract class Constraint implements Element, BooleanExpression {
 
   @Override
   public String declare() {
@@ -14,5 +15,10 @@ public abstract class Constraint implements Element {
    *         {@code constraint} keyword in the front and the semicolon in the end.
    */
   abstract String getExpression();
+
+  @Override
+  public String use() {
+    return getExpression();
+  }
 
 }
