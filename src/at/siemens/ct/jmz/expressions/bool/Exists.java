3213783ff9892e6d04a553009cb8de60237a60c2
Richard Taupe <richard.taupe@siemens.com>
2016-11-16 15:21:01 +0000

    feat: introduce Exists

---
+++ src/at/siemens/ct/jmz/expressions/bool/Exists.java
@@ -0,0 +1,40 @@
+package at.siemens.ct.jmz.expressions.bool;
+
+import at.siemens.ct.jmz.expressions.Expression;
+import at.siemens.ct.jmz.expressions.comprehension.Generator;
+import at.siemens.ct.jmz.expressions.comprehension.IteratorExpression;
+
+/**
+ * Represents an exists-expression, i.e. a disjunction of boolean expressions.
+ * 
+ * @author z003ft4a (Richard Taupe)
+ *
+ */
+public class Exists<T> implements BooleanExpression {
+
+	private final Generator<T> generator;
+  private final Expression<Boolean> expression;
+
+	public Exists(Generator<T> generator, Expression<Boolean> expression) {
+    this.generator = generator;
+    this.expression = expression;
+  }
+
+	public Exists(IteratorExpression<T> iterator, Expression<Boolean> expression) {
+    this(new Generator<T>(iterator), expression);
+  }
+
+  @Override
+  public String use() {
+		return String.format("exists(%s)(%s)", generator.use(), expression.use());
+  }
+
+	public Generator<T> getGenerator() {
+    return generator;
+  }
+
+  public Expression<Boolean> getExpression() {
+    return expression;
+  }
+
+}
