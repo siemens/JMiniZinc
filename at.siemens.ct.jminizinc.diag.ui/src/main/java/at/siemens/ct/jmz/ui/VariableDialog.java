e1eb10ad0b11dd6b766546f90fca189b88693330
Mara Ioana Rosu <mara.rosu@siemens.com>
2017-01-23 10:09:12 +0200

    style:
    	- copyright character deleted
    
    refactoring:
     
    	- duplicate files deleted
    	- minimalDiagnoses4 file moved in diag/testFiles

--- at.siemens.ct.jminizinc.diag.ui/src/main/java/at/siemens/ct/jmz/ui/VariableDialog.java
+++ at.siemens.ct.jminizinc.diag.ui/src/main/java/at/siemens/ct/jmz/ui/VariableDialog.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) Siemens AG, 2016
+ * Copyright Siemens AG, 2016
  * 
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
  * If a copy of the MPL was not distributed with this file, You can obtain one at
